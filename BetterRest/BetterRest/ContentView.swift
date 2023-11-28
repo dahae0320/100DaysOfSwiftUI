5577cde71170413224bf535eb637893d7bb8bdb4
dahae0320 <ldh990320ldh@gmail.com>
2023-11-28 17:16:57 +0900

    [Chore] Day 28 Challenge: BetterRest 업그레이드
    * VStack -> Section 변경
    * 먹은 커피 수를 표기하는 Stepper -> Picker 변경
    * 추천 수면 시간을 실시간으로 보여주도록 변경, `calculate` 버튼 삭제

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -12,16 +12,13 @@ struct ContentView: View {
     @State private var wakeUp = defaultSleepTime
     @State private var sleepAmout = 8.0
     @State private var coffeeAmount = 1
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
     
     static var defaultSleepTime: Date {
-        var component = DateComponents(hour: 7, minute: 0)
+        let component = DateComponents(hour: 7, minute: 0)
         return Calendar.current.date(from: component) ?? .now
     }
     
-    func calculateBedtime() {
+    var calculateBedtime: String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -33,47 +30,47 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: Int64(hour+minute), estimatedSleep: sleepAmout, coffee: Int64(coffeeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
             
-            alertTitle = "Your ideal bedtime is…"
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            return sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "Sorry, there was a problem calculating your bedtime."
         }
-        showingAlert = true
     }
     
     var body: some View {
         NavigationStack {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
+                Section("When do you want to wake up?") {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amout of sleep")
-                        .font(.headline)
+                Section("Desired amout of sleep") {
                     Stepper("\(sleepAmout.formatted()) hours", value: $sleepAmout, in: 4...12, step: 0.25)
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 0...20)
-//                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 0...20)
+                Section("Daily coffee intake") {
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(0..<21) { number in
+                            Text("^[\(number) cup](inflect: true)")
+                        }
+                    }
+                }
+                
+                Section {
+                    HStack {
+                        Spacer()
+                        VStack (spacing: 30) {
+                            Text("Your bedtime is...")
+                                .font(.subheadline)
+                            Text(calculateBedtime)
+                                .font(.largeTitle)
+                                .bold()
+                        }
+                        Spacer()
+                    }
+                    .padding(20)
                 }
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-                Button("calculate", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") { }
-            } message: {
-                Text("\(alertMessage)")
-            }
         }
     }
 }
