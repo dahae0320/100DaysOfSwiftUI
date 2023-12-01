ba036fe9d86c56b05e00f32bd409e1274b796550
dahae0320 <ldh990320ldh@gmail.com>
2023-12-01 11:37:17 +0900

    [Modify] notion-github sync test

--- .github/scripts/sync.py
+++ .github/scripts/sync.py
@@ -1,25 +1,28 @@
+from notion_client import Client
 import requests
 import os
 
-NOTION_API_TOKEN = os.getenv('API_TOKEN_NOTION')
-GITHUB_API_TOKEN = os.getenv('API_TOKEN_GITHUB')
-NOTION_PAGE_ID = 'YOUR_NOTION_PAGE_ID'  # Notion 페이지의 ID를 입력하세요
-REPO_OWNER = 'YOUR_GITHUB_REPO_OWNER'  # GitHub 레포지토리의 소유자를 입력하세요
-REPO_NAME = 'YOUR_GITHUB_REPO_NAME'  # GitHub 레포지토리의 이름을 입력하세요
+NOTION_API_TOKEN = os.getenv('NOTION_API_TOKEN')
+GITHUB_API_TOKEN = os.getenv('GITHUB_API_TOKEN')
+NOTION_PAGE_ID = '${{ vars.NOTION_PAGE_ID }}'
+REPO_OWNER = '${{ vars.REPO_OWNER }}'
+REPO_NAME = '${{ vars.REPO_NAME }}'
+REPO_README_PATH = '${{ vars.REPO_README_PATH }}'
 
-# Notion API 호출을 위한 헤더 설정
-notion_headers = {
-    'Authorization': f'Bearer {NOTION_API_TOKEN}',
-    'Content-Type': 'application/json',
-    'Notion-Version': '2023-08-01',  # 현재 Notion API 버전
-}
+# Notion API 호출을 위한 클라이언트 생성
+notion_client = Client(auth=NOTION_API_TOKEN)
 
-# Notion 페이지의 내용을 가져오는 함수
 def get_notion_content():
-    notion_url = f'https://api.notion.com/v1/pages/{NOTION_PAGE_ID}'
-    response = requests.get(notion_url, headers=notion_headers)
-    data = response.json()
-    return data['properties']['title']['title'][0]['text']['content']  # 예제에서는 제목만 가져옴
+    # Notion 페이지의 내용을 가져오기
+    page = notion_client.get_block(NOTION_PAGE_ID)
+
+    # 페이지의 텍스트 블록들을 Markdown으로 변환
+    markdown_content = ''
+    for block in page.children:
+        if block.type == 'text':
+            markdown_content += f'{block.title}\n'
+
+    return markdown_content
 
 # GitHub API 호출을 위한 헤더 설정
 github_headers = {
@@ -29,13 +32,25 @@ github_headers = {
 
 # GitHub README를 업데이트하는 함수
 def update_github_readme(content):
-    github_url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/readme'
+    github_url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/{REPO_README_PATH}/README.md'
+    
+    # GitHub API를 사용하여 README 파일 정보 가져오기
+    response = requests.get(github_url, headers=github_headers)
+    response.raise_for_status()
+    readme_info = response.json()
+
+    # 가져온 README 정보에서 sha 값 추출
+    current_sha = readme_info['sha']
+
+    # GitHub README를 업데이트하는 함수
     readme_content = {
-        'message': 'Update README from Notion',
+        'message': '[Sync] Update README from Notion',
         'content': content,
-        'sha': 'YOUR_CURRENT_README_SHA',  # 현재 README의 SHA 값을 입력하세요
+        'sha': current_sha,  # 동적으로 가져온 sha 값 사용
     }
+    # GitHub API를 사용하여 README 파일 업데이트
     response = requests.put(github_url, headers=github_headers, json=readme_content)
+    response.raise_for_status()
     return response.status_code
 
 if __name__ == '__main__':
