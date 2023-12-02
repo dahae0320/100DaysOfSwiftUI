4f215b7502e13343097f19914214a9f84dc325a7
dahae0320 <ldh990320ldh@gmail.com>
2023-12-02 15:46:51 +0900

    [Chore] Day 31: WordScramble Challenge * 입력받은 단어의 개수는 3글자 이상만 들어올 수 있도록 한다. * 입력받은 단어는 rootWord와 같아서는 안된다. * 점수 시스템을 구현한다.

--- Day 26-34 Expanding your skills/WordScramble/WordScramble/ContentView.swift
+++ Day 26-34 Expanding your skills/WordScramble/WordScramble/ContentView.swift
@@ -14,26 +14,28 @@ struct ContentView: View {
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
+    @State private var sumOfWordLength = 0
+    @State private var score = 0
     
     func addNewWord() {
         /// 1. `newWord`를 lowercase로 만들고 공백 제거하기
         let word = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        /// 2. `word`가 1개 이상의 문자로 이루어졌는지 확인하기
-        guard !word.isEmpty else {
-            wordError(title: "Empty Word", message: "You have to input more than 1 character.")
+        /// 2. `word`가 3개 이상의 문자로 이루어졌는지, 중복되지 않는지, 가능한 단어인지, 실제 있는 단어인지
+        guard word.count > 2 else {
+            wordError(title: "Empty Word", message: "You have to input more than 3 character.")
             return
         }
-        guard isOriginal(word: word) else {
+        guard isOriginal(word) else {
             wordError(title: "Word used already", message: "Be more original.")
             return
         }
 
-        guard isPossible(word: word) else {
+        guard isPossible(word) else {
             wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
             return
         }
 
-        guard isReal(word: word) else {
+        guard isReal(word) else {
             wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
             return
         }
@@ -41,7 +43,10 @@ struct ContentView: View {
         withAnimation {
             usedWords.insert(word, at: 0)
         }
-        /// 4. `newWord` 초기화하기
+        /// 4. 점수 계산하기
+        sumOfWordLength += newWord.count
+        score = sumOfWordLength * usedWords.count
+        /// 5. `newWord` 초기화하기
         newWord = ""
     }
     
@@ -58,13 +63,17 @@ struct ContentView: View {
         let allWords = startWords.components(separatedBy: "\n")
         /// 4. `rootWord`에 할당할 임의의 단어를 하나 선택하거나, 배열이 비어 있는 경우 기본값을 사용한다.
         rootWord = allWords.randomElement() ?? "slikworm"
+        /// 5. reset
+        sumOfWordLength = 0
+        score = 0
+        usedWords = []
     }
     
-    func isOriginal(word: String) -> Bool {
-        !usedWords.contains(word)
+    func isOriginal(_ word: String) -> Bool {
+        !usedWords.contains(word) && word != rootWord
     }
     
-    func isPossible(word: String) -> Bool {
+    func isPossible(_ word: String) -> Bool {
         var tempWord = rootWord
         for letter in word {
             if let pos = tempWord.firstIndex(of: letter) {
@@ -76,7 +85,7 @@ struct ContentView: View {
         return true
     }
     
-    func isReal(word: String) -> Bool {
+    func isReal(_ word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
@@ -95,6 +104,16 @@ struct ContentView: View {
                 Section {
                     TextField("Enter your word", text: $newWord)
                         .textInputAutocapitalization(.never)
+                }
+                Section {
+                    VStack(alignment: .leading) {
+                        Text("Score: \(score)")
+                            .font(.title3)
+                            .bold()
+                            .padding([.top, .bottom])
+                        Text("The score is calculated as [the sum of the length of each word * the total number of words].")
+                            .font(.caption2)
+                    }
                 }
                 Section {
                     ForEach(usedWords, id: \.self) { word in
